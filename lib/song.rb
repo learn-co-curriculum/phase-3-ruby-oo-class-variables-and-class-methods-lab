07aa2c81d789f37ae99fdb7aa5444f82e170eb28
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-24 08:40:56 +0300

    merge

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     def self.genre_count
       genre_count = {}
       @@genres.each do |genre|
-        
+
        if genre_count[genre]
           genre_count[genre] += 1
        else

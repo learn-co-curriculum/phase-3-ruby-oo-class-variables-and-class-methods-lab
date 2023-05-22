b93f60977ca979c8c637f7aa6d5b5306aeab5471
Meshack Korir <meshackkorir100@gmail.com>
2023-05-22 15:40:24 +0300

    several

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+    attr_accessor :name, :artist, :genre
+  
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+    
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def song.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+    
+end    

4c68286a7b1f5b0c1e4da849066aafa3835c816c
Meshack Korir <meshackkorir100@gmail.com>
2023-05-22 15:44:36 +0300

    several again

--- lib/song.rb
+++ lib/song.rb
@@ -27,4 +27,17 @@ class Song
         @@artists.uniq
     end
     
+    def self.genre_count
+        genre_count = Hash.new(0)
+        @@genres.each { |genre| genre_count[genre] += 1 }
+        genre_count
+    end
+
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each { |artist| artist_count[artist] += 1 }
+    artist_count
+  end
+  
 end    
